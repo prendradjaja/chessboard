5e3ffdede212dda8da09b9b6f8282586865850b1
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2022-04-18 11:53:18 -0500

    Draw pieces on the board

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,14 @@ const $$ = document.querySelectorAll.bind(document);
 
 const BOARD_SIZE = 8; // height & width in squares -- duplicated in styles.css
 
+type Position = PlacedPiece[]; // TODO name?
+
+interface PlacedPiece {
+  type: 'k' | 'q' | 'r' | 'b' | 'n' | 'p';
+  color: 'w' | 'b';
+  coordinates: [number, number];
+}
+
 function main() {
   new Chessboard('#my-board');
 }
@@ -18,16 +26,30 @@ class Chessboard {
   private mainLayer!: HTMLElement;
   private coordinatesLayer!: HTMLElement;
 
+  private squares: Partial<Record<string, HTMLElement>>; // What type should I use?
+
+  // private piecesLayer!: HTMLElement;
+
   constructor(selector: string) {
     const root = $(selector);
     if (!root) {
       throw new Error('Root element not found');
     }
     this.root = root as HTMLElement;
-    this.render();
+    this.squares = {};
+    this.renderBoard();
+
+    this.renderPosition([
+      { type: 'k', color: 'w', coordinates: [0, 0] },
+      { type: 'n', color: 'b', coordinates: [2, 1] },
+    ]);
   }
 
-  private render(): void {
+  private renderBoard(): void {
+    // // <div class="pieces-layer">
+    // this.piecesLayer = document.createElement('div');
+    // this.root.appendChild(this.piecesLayer);
+
     // <div class="main-layer">
     this.mainLayer = document.createElement('div');
     this.root.appendChild(this.mainLayer);
@@ -40,6 +62,7 @@ class Chessboard {
         this.mainLayer.appendChild(square);
         const squareColor = (r + c) % 2 === 0 ? 'light' : 'dark';
         square.classList.add('square', squareColor);
+        this.squares[`${r}-${c}`] = square;
       }
     }
     // </div.main-layer>
@@ -48,6 +71,26 @@ class Chessboard {
     this.coordinatesLayer = document.createElement('div');
     this.root.appendChild(this.coordinatesLayer);
     this.coordinatesLayer.classList.add('coordinates-layer');
+  }
+
+  private renderPosition(position: Position): void {
+    this.clearBoard();
+    for (let piece of position) {
+      const squareEl = this.getSquareElement(piece.coordinates[0], piece.coordinates[1]);
+      const pieceEl = document.createElement('img');
+      squareEl.appendChild(pieceEl);
+      pieceEl.src = `./images/pieces/${piece.color}${piece.type}.svg`;
+    }
+  }
+
+  private clearBoard(): void {
+    for (let square of Object.values(this.squares)) {
+      square!.innerHTML = ''; // Why do I need to tell TS that square is not undefined?
+    }
+  }
+
+  private getSquareElement(r: number, c: number): HTMLElement {
+    return this.squares[`${r}-${c}`]!;
   }
 }
 
